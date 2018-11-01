74024686aa6c7d7716778675859650604ee4592a
Isaac Fonseca <isaacfo1010@gmail.com>
2018-11-01 14:53:11 -0400

    added initial files and tables

---
+++ schema.sql
@@ -0,0 +1,39 @@
+-- create those tables!
+
+create table quizzes (
+    id serial primary key,
+    name varchar(300)
+
+);
+
+
+
+create table questions (
+    id serial primary key,
+    question_text text,
+    correct_answer text,
+    quiz_id integer references quizzes (id)
+
+);
+
+
+
+create table students (
+    id serial primary key,
+    name varchar (300)
+
+
+);
+
+
+
+
+create table answers (
+    is serial primary key,
+    answer_text text,
+    question_id integer references questions (id),
+    student_id integer references students (id)
+
+
+
+);
